b8af92743981fd4f2b2194b1f22ee3453d1f0c56
alkfjlkaj <ksjljlja@ssdf.df>
2021-11-15 15:36:55 -0500

    LegendsOfValor

--- src/legends/LOVGrid.java
+++ src/legends/LOVGrid.java
@@ -110,7 +110,7 @@ public class LOVGrid extends Grid {
         }else{
             return "     ";
         }
-        
+
     }
 
 
---
+++ src/legends/games/LegendsOfValor.java
@@ -0,0 +1,19 @@
+package legends.games;
+
+import legends.characters.heroes.Hero;
+
+import java.util.List;
+
+
+public class LegendsOfValor extends RPGGame {
+    public void playGame(){
+        //TODO
+    }
+
+    public List<Hero> getHeroes(){
+
+        //todo
+
+        return null;
+    }
+}
