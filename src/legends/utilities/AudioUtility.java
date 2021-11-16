69c35a34aa689cb588a623dcd5b061dddd390caa
alkfjlkaj <ksjljlja@ssdf.df>
2021-11-16 15:09:29 -0500

    made AudioUtility working

--- src/legends/utilities/AudioUtility.java
+++ src/legends/utilities/AudioUtility.java
@@ -1,41 +1,41 @@
 package legends.utilities;
 
-
-import legends.Main;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.io.File;
 
+/**
+ * @author
+ *
+ * A utility for audios and sounds
+ *
+ *
+ */
 public class AudioUtility {
     private static final String WORKING_DIRECTORY=System.getProperty("user.dir");
     private static final String PATH_TO_AUDIOS=WORKING_DIRECTORY+"/audios/";
 
-    private static final String FILE ="mixkit-martial-arts-fast-punch-2047.wav";
+    private static final String AUDIO_FILE ="mixkit-martial-arts-fast-punch-2047.wav";
 
 
-    public static synchronized void playSound() {
-        new Thread(new Runnable() {
-            // The wrapper thread is unnecessary, unless it blocks on the
-            // Clip finishing; see comments.
-            public void run() {
-                try {
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            Main.class.getResourceAsStream(PATH_TO_AUDIOS+FILE));
-                    clip.open(inputStream);
-                    clip.start();
-                } catch (Exception e) {
-                    System.out.println(PATH_TO_AUDIOS+FILE);
-                    System.err.println(e.getMessage());
-                }
-            }
-        }).start();
+    public static void sound() {
+        try {
+            //System.out.println(PATH_TO_AUDIOS+AUDIO_FILE);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(PATH_TO_AUDIOS+AUDIO_FILE));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            // If you want the sound to loop infinitely, then put: clip.loop(Clip.LOOP_CONTINUOUSLY);
+            // If you want to stop the sound, then use clip.stop();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
 
     public static void main(String[] args) {
-        AudioUtility.playSound();
+        AudioUtility.sound();
     }
 
 }
